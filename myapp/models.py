63a78c223caaf0a29dc712ec2d2c104839cc46f7
hashaem <hashemalsaie0457@gmail.com>
2025-08-11 08:27:06 +0300

    Models  learning

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    auther = models.ForeignKey(Author, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    birth_date = models.DateField(null=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
